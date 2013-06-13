4fca0be2574663b413cccbb2eeb7e09a96fd91f2
Werner Hanekom <wernerhanekom03@gmail.com>
2013-06-13 12:11:21 +0200

    part5

--- js/main.js
+++ js/main.js
@@ -9,24 +9,25 @@ var app = {
     },
 
     findByName: function() {
-        console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            $('.employee-list').html(self.employeeLiTpl(employees));
             }
         });
     },
 
+    renderHomeView: function() {
+       $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName(), this));
+    },
+
     initialize: function() {
         var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTple = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
