693bb35d524ae8eed8fa00c535720eb132b0a112
Werner Hanekom <wernerhanekom03@gmail.com>
2013-06-13 12:16:01 +0200

    part5working

--- js/main.js
+++ js/main.js
@@ -12,19 +12,18 @@ var app = {
         var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
             $('.employee-list').html(self.employeeLiTpl(employees));
-            }
         });
     },
 
     renderHomeView: function() {
-       $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName(), this));
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
         var self = this;
         this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTple = Handlebars.compile($("#employee-li-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
             self.renderHomeView();
         });
