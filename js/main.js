129753a0af858d29e993e41898276f50ce44c168
Werner Hanekom <wernerhanekom03@gmail.com>
2013-06-13 11:49:06 +0200

    part4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 var app = {
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     findByName: function() {
-        console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
             var e;
@@ -13,11 +20,24 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
+    renderHomeView: function() {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
     }
 
 };
 
-app.initialize(MemoryStore);
+app.initialize();
